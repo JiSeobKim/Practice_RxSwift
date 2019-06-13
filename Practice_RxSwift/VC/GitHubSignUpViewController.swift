ab5ce6346fff978eab9fdc127434e582f7e495fe
Kim Ji Seob <jiseob812@gmail.com>
2019-06-13 18:15:02 +0900

    GitHub SignUp 셋팅

---
+++ Practice_RxSwift/VC/GitHubSignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GitHubSignUpViewController.swift
+//  Practice_RxSwift
+//
+//  Created by kimjiseob on 13/06/2019.
+//  Copyright © 2019 kimjiseob. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class GitHubSignUpViewController: UIViewController {
+    
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfPass: UITextField!
+    @IBOutlet weak var tfPassR: UITextField!
+    @IBOutlet weak var btnSignUp: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
